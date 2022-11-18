d382aee39a32de8c83fc70abbcb570fc4065a6b4
Adam Turaj <adamturaj2@gmail.com>
2022-11-18 12:09:24 -0500

    Don't know why these didn't commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,17 @@
 import Head from "next/head";
 import type { AppProps } from "next/app";
 
+import ReactGA from "react-ga";
+
 import "../styles/globals.css";
 
+const TRACKING_ID = "G-JZWWY2DCYE";
+
 const App = ({ Component, pageProps }: AppProps) => {
+  ReactGA.initialize(TRACKING_ID);
+
+  ReactGA.pageview("/");
+
   return (
     <>
       <Head>
